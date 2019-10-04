4d9671f17bb35b1ec399130842d8f27a49cea7de
xeraen <xeraen@The.CyberPul.se>
2019-10-03 19:37:53 -0500

    Adds web UI for adding links

--- app/server.rb
+++ app/server.rb
@@ -6,6 +6,9 @@ HackrLink::init
 
 module HackrLink
   class Server < Sinatra::Base
+    enable :inline_templates
+    enable :sessions
+
     set :root, "#{File.dirname(__FILE__)}"
 
     db = PG.connect(
@@ -16,13 +19,79 @@ module HackrLink
       :password => HackrLink::Config['database']['password']
     )
 
+    User = Struct.new(:id, :username, :password)
+    USERS = [
+      User.new(1, HackrLink::Config['authentication']['ryker']['username'], HackrLink::Config['authentication']['ryker']['password']),
+      User.new(2, HackrLink::Config['authentication']['xeraen']['username'], HackrLink::Config['authentication']['xeraen']['password'])
+    ]
+
+    ##########################################################################
+    # Web UI
+    ##########################################################################
+
+    get '/login' do
+      erb :login
+    end
+
+    post '/login' do
+      user = USERS.find { |u| u.username == params[:username] }
+      if user && (params[:password] == user.password)
+        session.clear
+        session[:user_id] = user.id
+        redirect '/add_link'
+      else
+        @error = 'Username or password was incorrect'
+        erb :login
+      end
+    end
+
+    get '/add_link' do
+      if current_user
+        erb :add_link
+      else
+        redirect '/login'
+      end
+    end
+
+    post '/add_link' do
+      new_link_url = params[:new_link_url].to_s
+      redirect '/add_link' if new_link_url.empty?
+
+      select_sql = "SELECT * FROM links WHERE url IS NULL OR url = '' LIMIT 1;";
+      query_name = "#{Time.now.to_i}-#{SecureRandom.uuid}"
+      db.prepare query_name, select_sql
+      record = db.exec_prepared(query_name, []).first
+
+      update_sql = 'UPDATE links SET url = $1 WHERE id = $2;'
+      query_name = "#{Time.now.to_i}-#{SecureRandom.uuid}"
+      db.prepare query_name, update_sql
+      result = db.exec_prepared(query_name, [new_link_url, record['id'].to_i])
+
+      if result
+        url = "https://#{HackrLink::Config['system']['domain']}/#{record['shortcode']}"
+      else
+        @error = 'Could not create link'
+      end
+
+      erb :add_link
+    end
+
+    post '/logout' do
+      session.clear
+      redirect '/login'
+    end
+
+    ##########################################################################
+    # Actual hackr.link Functionality
+    ##########################################################################
+
     get '/:token?' do
       redirect_url = 'http://The.CyberPul.se'
       token = params[:token].to_s
 
       unless token.empty?
         sql = 'SELECT * FROM links WHERE shortcode = $1;';
-        query_name = "#{Time.now.to_i}-{SecureRandom.uuid}"
+        query_name = "#{Time.now.to_i}-#{SecureRandom.uuid}"
         db.prepare query_name, sql
         result = db.exec_prepared(query_name, [token])
         url = result.first['url'].to_s if result.count.positive?
@@ -31,5 +100,58 @@ module HackrLink
 
       redirect redirect_url
     end
+
+    helpers do
+      def current_user
+        return nil unless session[:user_id]
+        USERS.find { |u| u.id == session[:user_id] }
+      end
+    end
   end
 end
+
+__END__
+
+@@ login
+  <h1>Login</h1>
+  <% if @error %>
+    <p class="error"><%= @error %></p>
+  <% end %>
+  <form action="/login" method="POST">
+    <input name="username" placeholder="Username" />
+    <input name="password" type="password" placeholder="Password" />
+    <input type="submit" value="Login" />
+  </form>
+
+@@ add_link
+  <h1>Add Link</h1>
+  <p>Hello, <%= current_user.username %>.</p>
+  <form action="/logout" method="POST">
+    <input type="submit" value="Logout" />
+  </form>
+
+  <% if url %>
+    <ul>
+      <li><a href="<%= url %>" target="_blank"><%= url%></a></li>
+    </ul>
+  <% end %>
+
+  <h2>Add New Link</h2>
+  <form action="/add_link" method="POST">
+    <input name="new_link_url" placeholder="URL" />
+    <input type="submit" value="Add Link" />
+  </form>
+
+@@ layout
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8" />
+      <title>Simple Authentication Example</title>
+      <style>
+        input { display: block; }
+        .error { color: red; }
+      </style>
+    </head>
+    <body><%= yield %></body>
+  </html>
--- db/seed.rb
+++ db/seed.rb
@@ -15,9 +15,55 @@ legal_characters = %i{a b c d e f g h i j k l m n o p q r s t u v w x y z A B C 
 
 permutations = legal_characters.permutation(3).to_a
 
+sql = 'INSERT INTO links (shortcode, url, created_at, updated_at) VALUES ($1, $2, NOW(), NOW());'
+
 permutations.each do |permutation|
   query_name = "#{Time.now.to_i}-#{SecureRandom.uuid}"
-  sql = 'INSERT INTO links (shortcode, url, created_at, updated_at) VALUES ($1, $2, NOW(), NOW());'
   db.prepare query_name, sql
-  db.exec_prepared(query_name, [permutation.join(''), ''])
+  shortcode = permutation.join('')
+  if shortcode == 'bio'
+    db.exec_prepared(query_name, ['bio', 'http://The.CyberPul.se/bio'])
+  else
+    db.exec_prepared(query_name, [permutation.join(''), ''])
+  end
 end
+
+# hackr.link/yt
+query_name = "#{Time.now.to_i}-#{SecureRandom.uuid}"
+values = ['yt', 'https://youtube.com/channel/UCU6TswZ2C-QJ3jgf5ujhHNw']
+db.exec_prepared query_name, values
+
+# hackr.link/youtube
+query_name = "#{Time.now.to_i}-#{SecureRandom.uuid}"
+values = ['youtube', 'https://youtube.com/channel/UCU6TswZ2C-QJ3jgf5ujhHNw']
+db.exec_prepared query_name, values
+
+# hackr.link/twitter
+query_name = "#{Time.now.to_i}-#{SecureRandom.uuid}"
+values = ['twitter', 'https://twitter.com/TheCyberPulse']
+db.exec_prepared query_name, values
+
+# hackr.link/twitter
+query_name = "#{Time.now.to_i}-#{SecureRandom.uuid}"
+values = ['twitter', 'https://twitter.com/TheCyberPulse']
+db.exec_prepared query_name, values
+
+# hackr.link/instagram
+query_name = "#{Time.now.to_i}-#{SecureRandom.uuid}"
+values = ['instagram', 'https://instagram.com/TheCyberPulse']
+db.exec_prepared query_name, values
+
+# hackr.link/ig
+query_name = "#{Time.now.to_i}-#{SecureRandom.uuid}"
+values = ['ig', 'https://instagram.com/TheCyberPulse']
+db.exec_prepared query_name, values
+
+# hackr.link/xeraen
+query_name = "#{Time.now.to_i}-#{SecureRandom.uuid}"
+values = ['xeraen', 'http://xeraen.com']
+db.exec_prepared query_name, values
+
+# hackr.link/x
+query_name = "#{Time.now.to_i}-#{SecureRandom.uuid}"
+values = ['x', 'http://xeraen.com']
+db.exec_prepared query_name, values
