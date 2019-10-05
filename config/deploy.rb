aa372b50c0093b25f767aa39a4bb47a97d7dd8cf
xeraen <xeraen@The.CyberPul.se>
2019-10-04 22:07:51 -0500

    Removes linking of unused database.yml on deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,5 @@ set :repo_url, "git@github.com:TheCyberpulse/hackr.link.git"
 ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 set :deploy_to, "/var/www/hackr.link"
 append :linked_files, 'config/application.yml'
-append :linked_files, 'config/database.yml'
 set :keep_releases, 5
 set :ssh_options, { :forward_agent => true }
