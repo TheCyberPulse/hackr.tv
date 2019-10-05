dc9f7786f2ff1dd47b19f09132162b34fd13cd08
xeraen <xeraen@The.CyberPul.se>
2019-10-04 22:11:56 -0500

    Adds missing query preparations in seed.rb

--- db/seed.rb
+++ db/seed.rb
@@ -31,39 +31,47 @@ end
 # hackr.link/yt
 query_name = "#{Time.now.to_i}-#{SecureRandom.uuid}"
 values = ['yt', 'https://youtube.com/channel/UCU6TswZ2C-QJ3jgf5ujhHNw']
+db.prepare query_name, sql
 db.exec_prepared query_name, values
 
 # hackr.link/youtube
 query_name = "#{Time.now.to_i}-#{SecureRandom.uuid}"
 values = ['youtube', 'https://youtube.com/channel/UCU6TswZ2C-QJ3jgf5ujhHNw']
+db.prepare query_name, sql
 db.exec_prepared query_name, values
 
 # hackr.link/twitter
 query_name = "#{Time.now.to_i}-#{SecureRandom.uuid}"
 values = ['twitter', 'https://twitter.com/TheCyberPulse']
+db.prepare query_name, sql
 db.exec_prepared query_name, values
 
 # hackr.link/twitter
 query_name = "#{Time.now.to_i}-#{SecureRandom.uuid}"
 values = ['twitter', 'https://twitter.com/TheCyberPulse']
+db.prepare query_name, sql
 db.exec_prepared query_name, values
 
 # hackr.link/instagram
 query_name = "#{Time.now.to_i}-#{SecureRandom.uuid}"
 values = ['instagram', 'https://instagram.com/TheCyberPulse']
+db.prepare query_name, sql
 db.exec_prepared query_name, values
 
 # hackr.link/ig
 query_name = "#{Time.now.to_i}-#{SecureRandom.uuid}"
 values = ['ig', 'https://instagram.com/TheCyberPulse']
+db.prepare query_name, sql
 db.exec_prepared query_name, values
 
 # hackr.link/xeraen
 query_name = "#{Time.now.to_i}-#{SecureRandom.uuid}"
 values = ['xeraen', 'http://xeraen.com']
+db.prepare query_name, sql
 db.exec_prepared query_name, values
 
 # hackr.link/x
 query_name = "#{Time.now.to_i}-#{SecureRandom.uuid}"
 values = ['x', 'http://xeraen.com']
+db.prepare query_name, sql
 db.exec_prepared query_name, values
